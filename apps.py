73f684a1175d4ffb8c6c535e03402121b7ffa957
LilJack118 <kamilzajaczek4@gmail.com>
2022-04-02 20:04:16 +0200

    initial commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WagtailDynamicDropdownConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wagtail_dynamic_dropdown'
